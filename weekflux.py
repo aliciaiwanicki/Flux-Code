7dfd17b4403e18bb3640f2adff729720fe963f60
Eric <eric.s.russell@wsu.edu>
2020-02-19 14:54:04 -0800

    Update weekflux.py
    See comments in code and need to change back the input file path.

--- weekflux.py
+++ weekflux.py
@@ -5,8 +5,8 @@ import numpy as np
 import matplotlib.dates as mdates
 import matplotlib.ticker as plticker
 
-
-noniso = pd.read_csv('/Volumes/WSU/2. ALL DATA FILES/(5) Combined SFP Files/SFP output files in STD (wheat)/noniso_20200127_20200203.txt', engine='python', delimiter='\t', header = 0)
+#%%
+noniso = pd.read_csv(r'C:\Users\Eric\Desktop\Students\Alicia_I\noniso_20200127_20200203.txt', engine='python', delimiter='\t', header = 0)
 noniso.index = pd.to_datetime(noniso.Date_IV)
 
 noniso.loc[~(noniso['Lin_Flux[2]'] > 0), 'Lin_Flux[2]'] = np.nan            # Clip negative FLUX values to NaN
@@ -17,5 +17,9 @@ noniso.loc[~(noniso['Exp_R2[2]'] > 0.70), 'Exp_Flux[2]'] = 0
 
 LinExp = noniso['Lin_R2[2]'] >= noniso['Exp_R2[2]']
 N2O_Flux = []; N2O_Flux = pd.DataFrame(N2O_Flux)
-N2O_Flux = noniso['Exp_Flux[2]']; N2O_Flux = noniso['Lin_Flux[2]'][LinExp]
+
+#New Stuff; creates a column called 'Flux' as part of this.
+N2O_Flux['Flux'] = noniso['Exp_Flux[2]'] # Sets the entire column equal to Exp_Flux[2] data
+N2O_Flux['Flux'][LinExp] = noniso['Lin_Flux[2]'] # Sets just the times when LinExp == True to the Lin_Flux[2] data; should just replace those few columns
+# End of new stuff
 print(sum(LinExp))
